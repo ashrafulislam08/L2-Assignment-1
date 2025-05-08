6a0210f746c634833e30a6f6f9914e7148bb85dc
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-05-08 09:51:41 +0600

    done problem 2

--- index.ts
+++ index.ts
@@ -2,3 +2,15 @@ function formatString(input: string, toUpper?: boolean): string {
     return toUpper || typeof toUpper === "undefined" ? input.toUpperCase() : input.toLowerCase();
 }
 
+
+function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
+    const filtered = items.filter(item => item.rating >= 4)
+    return filtered
+}
+
+const books = [
+    { title: "Book A", rating: 4.5 },
+    { title: "Book B", rating: 3.2 },
+    { title: "Book C", rating: 5.0 }
+];
+
