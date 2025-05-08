0a7c93559f549d87057bae1547d737c656b44dbe
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-05-08 10:36:37 +0600

    done problem 4

--- index.ts
+++ index.ts
@@ -12,3 +12,33 @@ function filterByRating(items: { title: string; rating: number }[]): { title: st
 function concatenateArrays<T>(...arrays: T[][]): T[] {
     return arrays.flat()
 }
+
+
+class Vehicle {
+    private make: string;
+    year: number;
+
+    constructor(make: string, year: number) {
+        this.make = make;
+        this.year = year;
+    }
+
+
+    getInfo() {
+        console.log(`Make: ${this.make}, Year: ${this.year}`)
+    }
+}
+
+
+class Car extends Vehicle{
+    model: string;
+    constructor(make: string, year: number, model: string) {
+        super(make, year)
+        this.model = model
+    }
+
+    getModel() {
+        console.log(`Model: ${this.model}`)
+    }
+}
+
